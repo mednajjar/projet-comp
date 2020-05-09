df933c5009da35d585858dbe000b23a675a6af00
Mohammed Najjar <najjarmedd@gmail.com>
2020-05-09 08:21:28 +0000

    add progress & counter code with first test result code

--- Starter-file/dist/js/script.js
+++ Starter-file/dist/js/script.js
@@ -4,6 +4,7 @@ let demarer = document.getElementById('btn_test');
 let cer1 = document.getElementById('cer1');
 let cer2 = document.getElementById('cer2');
 let cer3 = document.getElementById('cer3');
+let final = document.getElementById('resultatFinal')
 
 demarer.addEventListener('click', show)
 
@@ -13,6 +14,8 @@ function show() {
     demarer.style.display = 'none';
     cer1.style.background = 'none';
     cer2.style.background = '#1078AD';
+    final.style.display = 'none'
+    step(conter, valeur);
 }
 
 function buildQuiz() {
@@ -85,7 +88,20 @@ function buildQuiz() {
 // show number of correct answers out of total
 //     resultsContainer.innerHTML = `${numCorrect} out of ${myQuestions.length}`;
 // }
+//:::::::::::::::::::::::::::::::::::::::::::::::::::::::
+let myvar = 1;
+let qNumber = document.getElementById('qNumber');
 
+function increment() {
+
+    myvar++;
+    qNumber.innerHTML = myvar + '/22';
+}
+
+function decrement() {
+    myvar--;
+    qNumber.innerHTML = myvar + '/22';
+}
 
 
 //:::::::variables::::::::::::::::::::::::::
@@ -125,7 +141,131 @@ function showSlide(n) {
 showSlide(0);
 
 
+//:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+var resultat = [];
+tabSymptome = [];
+tabfacteurPronostique = [];
+tabfacteurMineur = [];
+tabfacteurMajeur = [];
 
+resultSyptome = 0;
+resultFacteurPronostique = 0;
+resultfacteurMineur = 0;
+resultfacteurMajeur = 0;
+
+trireponces = () => {
+    for (let i = 0; i < resultat.length; i++) {
+        if (i == 1) {
+            tabfacteurMineur.push(resultat[i]);
+            tabfacteurMajeur.push(resultat[i]);
+        } else if (i >= 11 && i <= 21) {
+            tabfacteurPronostique.push(resultat[i]);
+        } else if (i == 7 || i == 8) {
+            tabfacteurMajeur.push(resultat[i]);
+        } else if (i == 6 || i == 9) {
+            tabfacteurMineur.push(resultat[i]);
+        }
+        if (i >= 0 && i <= 9) {
+            tabSymptome.push(resultat[i]);
+        }
+    }
+};
+
+nombreDeFacteur = () => {
+    for (let i = 0; i < tabSymptome.length; i++) {
+        if (tabSymptome[i] == "Oui") {
+            resultSyptome++;
+        }
+    }
+    for (let i = 0; i < tabfacteurPronostique.length; i++) {
+        if (tabfacteurPronostique[i] >= 70 || tabfacteurPronostique[i] == "Oui") {
+            resultFacteurPronostique++;
+        }
+    }
+    for (let i = 0; i < tabfacteurMineur.length; i++) {
+        if (
+            tabfacteurMineur[i] >= 39 ||
+            tabfacteurMineur[i] == "Oui" ||
+            tabfacteurMineur[i] == "Très fatigué" ||
+            tabfacteurMineur[i] == " fatigué"
+        ) {
+            resultfacteurMineur++;
+        }
+    }
+    for (let i = 0; i < tabfacteurMajeur.length; i++) {
+        if (tabfacteurMajeur[i] <= 35.4 || tabfacteurMajeur[i] == "Oui") {
+            resultfacteurMajeur++;
+        }
+    }
+};
+let messageFinal = document.getElementById("affichageResult");
+Algorithme = () => {
+    if (
+        resultat[0] == "Oui" ||
+        (resultat[2] == "Oui" && resultat[4] == "Oui") ||
+        (resultat[2] == "Oui" && resultat[3] == "Oui") ||
+        (resultat[0] == "Oui" && resultat[5] == "Oui")
+        //Patient avec fièvre, ou toux + mal de gorge, ou toux + courbatures ou fièvre + diarrhée :
+    ) {
+        if (resultFacteurPronostique == 0) {
+            if (
+                resultfacteurMajeur == 0 &&
+                resultfacteurMineur == 0 &&
+                resultat[10] < 50
+            ) {
+                messageFinal.innerText = messageAffiche[0];
+            } else if (
+                resultfacteurMajeur == 0 &&
+                resultfacteurMineur >= 1 &&
+                (resultat[10] >= 50 || resultat[10] <= 69)
+            ) {
+                messageFinal.innerText = messageAffiche[1];
+            }
+        } else {
+            if (resultfacteurMajeur == 0 && resultfacteurMineur <= 1) {
+                messageFinal.innerText = messageAffiche[1];
+            } else if (resultfacteurMajeur == 0 && resultfacteurMineur >= 2) {
+                messageFinal.innerText = messageAffiche[2];
+            }
+        }
+        if (resultfacteurMajeur >= 1) {
+            messageFinal.innerText = messageAffiche[2];
+        }
+    } else if (resultat[0] == "Oui" && resultat[2] == "Oui") {
+        if (resultFacteurPronostique == 0) {
+            if (resultfacteurMajeur == 0 && resultfacteurMineur >= 1) {
+                messageFinal.innerText = messageAffiche[3];
+            }
+        } else {
+            if (resultfacteurMajeur == 0 && resultfacteurMineur <= 1) {
+                messageFinal.innerText = messageAffiche[3];
+            } else if (resultfacteurMajeur == 0 && resultfacteurMineur > 1) {
+                messageFinal.innerText = messageAffiche[2];
+            }
+        }
+        if (resultfacteurMajeur > 0) {
+            messageFinal.innerText = messageAffiche[2];
+        }
+    } else if (
+        resultat[0] == "Oui" ||
+        resultat[2] == "Oui" ||
+        resultat[3] == "Oui" ||
+        resultat[4] == "Oui"
+    ) {
+        if (resultfacteurMajeur == 0 && resultfacteurMineur == 0) {
+            messageFinal.innerText = messageAffiche[4];
+        } else {
+            if (resultFacteurPronostique > 0) {
+                messageFinal.innerText = messageAffiche[4] + " " + messageAffiche[2];
+            }
+        }
+    } else if (resultSyptome == 0) {
+        messageFinal.innerText = messageAffiche[5];
+    } else if (resultat[10] < 15) {
+        messageFinal.innerText = messageAffiche[6];
+    }
+};
+//::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 
 
 
@@ -141,30 +281,39 @@ function showNextSlide() {
     inputRadio.checked = false;
     showSlide(currentSlide + 1);
 
+    var v1 = document.getElementById('p1').value;
+    document.getElementById("p1").value = v1 + 1;
+
+    increment();
+
+
 
 
 }
 
 function showPreviousSlide() {
+
     showSlide(currentSlide - 1);
+    if (currentSlide >= 0) {
+        var v1 = document.getElementById('p1').value;
+        document.getElementById("p1").value = v1 - 1;
+    }
 
-
-
-
-
-
-
-
-
+    decrement();
 
 }
 
 function showResults() {
     let testo = document.getElementById('testo');
+
     if (currentSlide == totQuestion - 1) {
         testo.style.display = 'none';
         cer2.style.background = 'none';
-        cer3.style.background = '#96C5DC'
+        cer3.style.background = '#96C5DC';
+        final.style.display = 'block';
+        trireponces();
+        nombreDeFacteur();
+        Algorithme();
     }
 }
 
